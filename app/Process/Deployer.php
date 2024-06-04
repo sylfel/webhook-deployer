abad9c0af78e78d11e0fc55edc4fed7c7d67a35c
Jean-Philippe THEVENOUX <jp.thevenoux@pm.me>
2024-06-04 09:36:51 +0200

    add log

--- app/Process/Deployer.php
+++ app/Process/Deployer.php
@@ -3,6 +3,7 @@
 namespace App\Process;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Process;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob as SpatieProcessWebhookJob;
 
@@ -13,17 +14,21 @@ class Deployer extends SpatieProcessWebhookJob
         $payload = $this->webhookCall->payload;
         $repository = Arr::get($payload, 'repository.name');
         if (!$repository) {
+            Log::notice('Deployer (id : {id}) - no repository ', ['id' => $this->webhookCall->id]);
             return;
         }
         $pathSites = config('app.home');
-        $homeDir = $pathSites . $repository;
-        if (!file_exists($homeDir)) {
+        $homePath = $pathSites . $repository;
+        if (!file_exists($homePath)) {
+            Log::notice('Deployer (id : {id}) - Path not exists {path}', ['id' => $this->webhookCall->id, 'path' => $homePath]);
             return;
         }
-        $scriptPath = $homeDir . '/.scripts/deploy.sh';
+        $scriptPath = $homePath . '/.scripts/deploy.sh';
         if (!file_exists($scriptPath)) {
+            Log::notice('Deployer (id : {id}) - Script not exists {path}', ['id' => $this->webhookCall->id, 'path' => $scriptPath]);
             return;
         }
+        Log::notice('Deployer (id : {id}) - Run {path}', ['id' => $this->webhookCall->id, 'path' => $scriptPath]);
         Process::run($scriptPath);
     }
 }
