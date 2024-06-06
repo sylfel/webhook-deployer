e4d32a9480a33bef6f62f4dbcbb6570bfaf60c46
Jean-Philippe THEVENOUX <jp.thevenoux@pm.me>
2024-06-06 11:55:32 +0200

    add fetching github action ip range

---
+++ app/Console/Commands/RefreshGithubIP.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\IpRange;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class RefreshGithubIP extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:refresh-github-ip';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Refresh github's action ip list";
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::get('https://api.github.com/meta');
+        if ($response->successful()) {
+            IpRange::truncate();
+            $starting_time = microtime(true);
+            $actionIps = collect($response->json()['actions']);
+            $ipRangeList = $actionIps->map(function (string $item) {
+                $range = \IPLib\Factory::parseRangeString($item);
+
+                return new IpRange([
+                    'adresseType' => $range->getAddressType(),
+                    'rangeFrom' => $range->getComparableStartString(),
+                    'rangeTo' => $range->getComparableEndString(),
+                ]);
+            });
+            IpRange::insert($ipRangeList->toArray());
+            $finished_time = microtime(true);
+            $time = ($finished_time - $starting_time); // time in sec
+            $this->comment("Process {$actionIps->count()} IP range in $time ");
+        }
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\RefreshGithubIP;
+use Illuminate\Queue\Console\WorkCommand;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(RefreshGithubIP::class)->daily();
+Schedule::command(WorkCommand::class, ['--stop-when-empty'])->everyMinute();
