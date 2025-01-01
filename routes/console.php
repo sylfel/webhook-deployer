f1bf41c46edf3970ca7791b3bd50bad5314969ca
THEVENOUX Jean-Philippe <jp.thevenoux@pm.me>
2025-01-01 19:20:07 +0100

    fix: homepage + prune model (#14)

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,10 @@
 use App\Console\Commands\RefreshGithubIP;
 use Illuminate\Queue\Console\WorkCommand;
 use Illuminate\Support\Facades\Schedule;
+use Spatie\WebhookClient\Models\WebhookCall;
 
 Schedule::command(RefreshGithubIP::class)->daily();
 Schedule::command(WorkCommand::class, ['--stop-when-empty'])->everyMinute();
+Schedule::command('model:prune', [
+    '--model' => [WebhookCall::class],
+])->daily();
